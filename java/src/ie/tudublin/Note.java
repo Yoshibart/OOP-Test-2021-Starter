38506e0b366f1f74c2c7eb71f2741abe287733d4
Barnet Obeka <oworibarnet@gmail.com>
2021-04-21 21:18:15 +0100

    added the Note Variable

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -3,15 +3,19 @@ package ie.tudublin;
 public class Note {
     private char note;
     private int duration;
+    private String type;
     
-    public Note(char note, int duration) {
+    public Note(char note, int duration, String type) {
         this.note = note;
         this.duration = duration;
+        this.type = type;
     }
 
     public String toString() {
-        return "Note [duration=" + duration + ", note=" + note + "]";
+        return "   " + duration + "     " + note + "      " + type + " ";
     }
+
+
 
     public char getNote() {
         return note;
@@ -19,6 +23,14 @@ public class Note {
 
     public void setNote(char note) {
         this.note = note;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public int getDuration() {
