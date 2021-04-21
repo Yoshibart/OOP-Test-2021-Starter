5f6bbfd180450264c24337efa9a2ed447a8ddd7a
Barnet Obeka <oworibarnet@gmail.com>
2021-04-21 21:19:55 +0100

    Drawing Music Notes

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -6,9 +6,9 @@ import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	// String score = "DEFGABcd";
+	String score = "DEFGABcd";
 	// String score = "D2E2F2G2A2B2c2d2";
-	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	// String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 	ArrayList<Note> note = new ArrayList<Note>();
 	
 	public void settings()
@@ -16,9 +16,9 @@ public class ScoreDisplay extends PApplet
 		size(1000, 500);
 
 		// How to convert a character to a number
-		char c = '7'; // c holds the character 7 (55)
-		int i = c - '0'; // i holds the number 7 (55 - 48) 
-		println(i);
+		// char c = '7'; // c holds the character 7 (55)
+		// int i = c - '0'; // i holds the number 7 (55 - 48) 
+		// println(i);
 	}
 
 	public void setup() 
@@ -26,58 +26,66 @@ public class ScoreDisplay extends PApplet
 	{	colorMode(HSB);
 		loadScore();
 		printScores();
-		drawLines();
 	}
 
 	public void draw()
 	{
-		// background(255);
-		
+		background(255);
+		drawLines();
+		drawNotes();
 	}
 
 	void drawNotes()
-	{
+	{	
+		float border = (float)width * 0.1f;
+		for(int i = 0; i < note.size(); i++){
+			float y = map(i, 0, note.size(), border * 2, width - (border * 2));
+			float y1 = map(i, 1, note.size(), height - border, border * 3);
+			fill(0);
+			text(note.get(i).getNote(), y, border * 2);
+			fill(0);
+			if(note.get(i).getType() == "Crotchet"){
+				circle(y, y1, 20);
+				line(y + 10, y1, y + 10, y1 - 50);
+			}
+			else{
+				circle(y, y1, 20);
+				line(y + 10, y1, y + 10, y1 - 50);
+				line(y + 10, y1 - 50, y + 20, y1 - 40);
+			}
+		}
 
+		
 	}
 
 
 	public void drawLines(){
 		background(255);
 		float border = (float)width * 0.1f;
-		// float cx = (float)width / 2;
 		for(int i = 1; i <= 5; i++){
-			float y = map(i, 1, 5, border * 2, height - border);
+			float y = map(i, 1, 5, border * 3, height - border);
 			stroke(0);
 			fill(0);
 			line(border * 2, y, width - (border * 2), y);
-		}
-		for(int i = 0; i < note.size(); i++){
-			float y = map(i, 0, note.size(), border * 2, width - (border * 2));
-			fill(0);
-			text(note.get(i).getNote(), y + (y * 0.05f), border);
 		}
 	}
 
 	public void loadScore(){
 
 		for(int i = 0; i < score.length() - 1; i++){
-			char c = score.charAt(i + 1); // c holds the character 7 (55)
-			int j = c - '0'; // i holds the number 7 (55 - 48) 
+			char c = score.charAt(i + 1);
+			int j = c - '0';
 			if(j == 2){
-				note.add(new Note(score.charAt(i), 2));
+				note.add(new Note(score.charAt(i), 2, "Crotchet"));
 				++i;
 			}else{
-				note.add(new Note(score.charAt(i), 1));
+				note.add(new Note(score.charAt(i), 1, "Quaver"));
 			}
 		}
 	}
 	public void printScores(){
 		for(Note n : note){
-			if(n.getDuration() > 1){
-				println(n.getNote() + "		" + n.getDuration() + "	 " + "Crotchet");
-			}else{
-				println(n.getNote() + "		" + n.getDuration() + "	 " + "Quaver");
-			}
+			println(n.toString());
 		}
 	}
 }
