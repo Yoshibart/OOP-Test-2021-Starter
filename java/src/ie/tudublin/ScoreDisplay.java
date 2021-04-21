eaa0e9748ef5d6552947f84c56aba5696e42495c
Barnet Obeka <oworibarnet@gmail.com>
2021-04-21 18:24:18 +0100

    Loading Strings into the Arraylist of type Note

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -2,14 +2,14 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
-
 import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
 	String score = "DEFGABcd";
-	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	// String score = "D2E2F2G2A2B2c2d2";
+	// String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	ArrayList<Note> note = new ArrayList<Note>();
 	
 	public void settings()
 	{
@@ -23,7 +23,8 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
+		printScores();
 	}
 
 	public void draw()
@@ -34,5 +35,21 @@ public class ScoreDisplay extends PApplet
 
 	void drawNotes()
 	{
+
 	}
+
+	public void loadScore(){
+
+		for(int i = 0; i < score.length() - 1; i++){
+			char c = score.charAt(i + 1); // c holds the character 7 (55)
+			int j = c - '0'; // i holds the number 7 (55 - 48) 
+			if(j == 2){
+				note.add(new Note(score.charAt(i), 2));
+				++i;
+			}else{
+				note.add(new Note(score.charAt(i), 1));
+			}
+		}
+	}
+	
 }
